e2ddc274bce77d544bf420a8dfd28add5326bbfb
Nawin Kumar Sharma <kumarsharmanawin@gmail.com>
2021-08-15 18:44:05 +0530

    Adding seperate training and visualization of variation of mean image in direction of PCA vectors

---
+++ pca_training.m
@@ -0,0 +1,66 @@
+clear all;
+
+%Initilize data
+imageFolder = "./dataset/";
+image_width = 200;
+image_height = 200;
+
+% Get list of all JPG files in this directory
+imagefiles = dir('./dataset/*.jpg');     
+numberOfImages = length(imagefiles);    % Number of files found
+
+%Initializing arrays 
+imageVectors = double(zeros(image_width*image_height*3,numberOfImages));
+sumImage = double(zeros(image_width,image_height,3));
+
+%loop to load images
+disp("Loading Image ...");
+for itr=1:numberOfImages
+   currentFileName = imagefiles(itr).name;
+   currenfullfilename = fullfile(imageFolder,currentFileName);
+   currentimage = double(imread(currenfullfilename));
+   imageVectors(:,itr) = reshape(currentimage,image_width*image_height*3,1);
+   sumImage += currentimage;
+end
+
+%Finding mean image
+meanImage = sumImage / numberOfImages;
+%imshow(uint8(meanImage));
+imwrite(uint8(meanImage),"meanImage.jpg");
+meanImageReshaped = reshape(meanImage,image_height*image_width*3,1);
+
+%loop for subtracting mean image from each image
+disp("Subtracting mean Image from all images ...");
+for itr=1:numberOfImages
+  imageVectors(:,itr) = imageVectors(:,itr) - meanImageReshaped;
+end
+
+%Applying PCA to zero meaned images
+%Finding Covariance matrix
+disp("Finding PCA ...");
+covImages = transpose(imageVectors)*imageVectors;
+%Finding Eigen vectors and Eigen values
+[Vtemp,mtemp] = eig(covImages);
+V = real(imageVectors*Vtemp);%Vectors of transformed PCA space
+m = diag(mtemp);%Eigen values of vectors
+
+%Saving 100 Principal vectors as array
+disp("saving PCA ...");
+V_100 = V(:,1:100);
+m_100 = m(1:100);
+save V_100.mat V_100;
+save m_100.mat m_100;
+save mean_image.mat meanImage
+
+%Saving 100 Principal vectors as images
+disp("Saving PCA vectors as Image ...");
+if ~exist("PCA_images", 'dir')
+   mkdir("PCA_images")
+end
+for itr=1:100
+  vec = reshape(V(:,itr),image_height*image_width*3,1);
+  vecNorm = 255*(vec - min(vec))/(max(vec)-min(vec));
+  vecImg = reshape(vecNorm,image_height,image_width,3);
+  fname = ["PCA_images/",num2str(itr),".jpg"];
+  imwrite(uint8(vecImg),fname);
+end
---
+++ pca_visualization.m
@@ -0,0 +1,27 @@
+clear all;
+
+%Initilize data
+vecNo = 16;%change from 1 to 100
+image_width = 200;
+image_height = 200;
+
+%Loading matrix made on PCA training
+disp("Loading PCA Vectors...");
+load V_100.mat
+disp("Loading PCA eigen values...");
+load m_100.mat
+disp("Loading PCA eigen values...");
+load mean_image.mat
+
+meanImageReshaped = reshape(meanImage,image_height*image_width*3,1);
+
+%Loop for seeing variation of mean image in direction of PCA vectors
+weight = [-1:0.1:1];
+for itr=0:20
+  imgVec = reshape(V_100(:,3),image_height*image_width*3,1);%3
+  imgVecNorm = 255*(imgVec - min(imgVec))/(max(imgVec)-min(imgVec));
+  imgBlend = weight(itr+1) * imgVecNorm + meanImageReshaped;
+  imgBlendNorm = 255*(imgBlend - min(imgBlend))/(max(imgBlend)-min(imgBlend));
+  img = reshape(imgBlendNorm,image_height,image_width,3);
+  imwrite(uint8(img),["try",num2str(itr),".jpg"]);
+end
